76a85b5475f97c6b0fd755fe1f10c409f1c1ad67
Alex Roytman <aroytman@profoundlogic.com>
2012-11-30 14:27:23 -0500

    Adjust positioning of spin buttons when css class input is not assigned to element

--- proddata/js/widgets/spinner.js
+++ proddata/js/widgets/spinner.js
@@ -40,17 +40,6 @@ pui.Spinner = function(dom, minValue, maxValue, increment, runtimeMode) {
   up.style.borderStyle = "none";
   up.style.backgroundColor = "transparent";
   up.src = pui.normalizeURL("/profoundui/proddata/images/up.gif");
-  var left = parseInt(dom.style.left);
-  if (isNaN(left)) left = 0;
-  var top = parseInt(dom.style.top);
-  if (isNaN(top)) top = 0;
-  up.style.left = left +  dom.offsetWidth - 16 + "px";
-  top += 1;
-  top += parseInt((dom.offsetHeight - 22) / 2);
-  if (!quirksMode) top += 1;
-  up.style.top = top + "px";
-  up.style.zIndex = dom.style.zIndex;
-  up.style.visibility = dom.style.visibility;
   up.style.cursor = "pointer";
   up.onclick = function() {
     me.spin(increment);
@@ -67,16 +56,6 @@ pui.Spinner = function(dom, minValue, maxValue, increment, runtimeMode) {
   down.style.borderStyle = "none";
   down.style.backgroundColor = "transparent";
   down.src = pui.normalizeURL("/profoundui/proddata/images/down.gif");
-  var left = parseInt(dom.style.left);
-  if (isNaN(left)) left = 0;
-  var top = parseInt(dom.style.top);
-  if (isNaN(top)) top = 0;
-  down.style.left = left +  dom.offsetWidth - 16 + "px";
-  top += 11;
-  top += parseInt((dom.offsetHeight - 22) / 2);
-  down.style.top = top + "px";
-  down.style.zIndex = dom.style.zIndex;
-  down.style.visibility = dom.style.visibility;
   down.style.cursor = "pointer";
   down.onclick = function() {
     me.spin(-increment);
@@ -113,6 +92,34 @@ pui.Spinner = function(dom, minValue, maxValue, increment, runtimeMode) {
   // Public Properties
   
   // Public Methods
+  this.positionSpinnButtons = function() {
+    var left = parseInt(dom.style.left);
+    if (isNaN(left)) left = 0;
+    var top = parseInt(dom.style.top);
+    if (isNaN(top)) top = 0;
+    up.style.left = left +  dom.offsetWidth - 15 + "px";
+    top += parseInt((dom.offsetHeight - 18) / 2);
+    if (quirksMode) top -= 1;
+    up.style.top = top + "px";
+    up.style.zIndex = dom.style.zIndex;
+    up.style.visibility = dom.style.visibility;
+
+    left = parseInt(dom.style.left);
+    if (isNaN(left)) left = 0;
+    top = parseInt(dom.style.top);
+    if (isNaN(top)) top = 0;
+    down.style.left = left +  dom.offsetWidth - 15 + "px";
+    top += 9;
+    top += parseInt((dom.offsetHeight - 18) / 2);
+    if (dom.offsetHeight % 2 == 1) {
+      if (dom.offsetHeight < 18) top -= 1;
+      else top += 1;
+    }
+    down.style.top = top + "px";
+    down.style.zIndex = dom.style.zIndex;
+    down.style.visibility = dom.style.visibility;
+  }
+  
   this.hide = function() {
     up.style.visibility = "hidden";
     down.style.visibility = "hidden";
@@ -170,6 +177,7 @@ pui.widgets.add({
       if (!parms.design) {
         setTimeout( function() { 
           parms.dom.spinner = new pui.Spinner(parms.dom, parms.evalProperty("min value"), parms.evalProperty("max value"), parms.evalProperty("increment value"), !parms.design);
+          parms.dom.spinner.positionSpinnButtons();
         }, 1);
       }
       if (parms.design) parms.dom.readOnly = true;
