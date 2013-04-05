ProfoundLogic/profoundui-framework

commit 2ab5b32121e6c91365df7e2711e47695832473d2

Allow JSON Array syntax for various types of choice lists

proddata/js/runtime/AutoComplete.js
@@ -860,7 +860,7 @@ function applyAutoComp(properties, originalValue, domObj) {
     var valueField = evalPropertyValue(properties["choice values field"], originalValue, domObj);
     var limit = evalPropertyValue(properties["max choices"], originalValue, domObj);
     var choices = evalPropertyValue(properties["choices"], originalValue, domObj);
-    choices = choices.split(",");
+    choices = pui.parseCommaSeparatedList(choices);
     var values = evalPropertyValue(properties["choice values"], originalValue, domObj);
     if (values == "") {
       values = [];
@@ -869,7 +869,7 @@ function applyAutoComp(properties, originalValue, domObj) {
       }
     }
     else {
-      values = values.split(",");
+      values = pui.parseCommaSeparatedList(values);
     }
 
     // Apply auto complete if any of the settings are given.

proddata/js/runtime/dspf/render.js
@@ -1022,7 +1022,7 @@ pui.renderFormat = function(parms) {
                 if (dbFile != null && dbFile != "") {
                   var optionFields = pui.evalBoundProperty(items[i]["choice options field"], data, parms.ref);
                   var optionField;
-                  if (optionFields != null) optionField = optionFields.split(",")[0];
+                  if (optionFields != null) optionField = pui.parseCommaSeparatedList(optionFields)[0];
                   var valueField = pui.evalBoundProperty(items[i]["choice values field"], data, parms.ref);
                   if (optionField != null && optionField != "" && valueField != null && valueField != "" && optionField != valueField) {
                     propValue["formatting"] = "Text";
@@ -2694,7 +2694,7 @@ pui.buildResponse = function() {
           }
         
           if (typeof dom.validValues == "string") {
-            var validValues = dom.validValues.split(",");
+            var validValues = pui.parseCommaSeparatedList(dom.validValues);
             var invalid = true;
             for (var i = 0; i < validValues.length; i++) {
               if (rtrim(validValues[i]) == value) {

proddata/js/runtime/utils.js
@@ -1378,4 +1378,25 @@ pui.getScript = function(path) {
   return null;
 }
 
+// syntax allowed:
+//   - string (comma separated): 'A,B,C'
+//   - string (json formatted): '["A","B","C"]'
+//   - javascript array
+pui.parseCommaSeparatedList = function(list) {
+  if (typeof list != "string") {
+    if (lsit instanceof Array) return list;
+    else return [];
+  }
+  if (list == "") return [];
+  var listArray;
+  if (list.substr(0, 1) == "[" && list.substr(list.length - 1, 1) == "]") {
+    try {
+      listArray = eval(list);
+    }
+    catch(e) {
+    }
+  }
+  if (listArray == null) listArray = listArray.split(",");
+  return listArray;
+}
 

proddata/js/widgets/chart.js
@@ -318,8 +318,8 @@ pui.widgets.add({
               chartXML += " " + chartOptions;
             }
             chartXML += '>';
-            var nameArray = nameList.split(",");
-            var valueArray = valueList.split(",");
+            var nameArray = pui.parseCommaSeparatedList(nameList);
+            var valueArray = pui.parseCommaSeparatedList(valueList);
             var n = nameArray.length;
             if (valueArray.length > n) n = valueArray.length;
             for (var i = 0; i < n; i++) {

proddata/js/widgets/combo_box.js
@@ -434,8 +434,6 @@ pui.widgets.add({
         return;
       }
       
-      //parms.dom.comboBoxWidget["choices"] = parms.evalProperty("choices").split(",");
-      //parms.dom.comboBoxWidget["choice values"] = parms.evalProperty("choice values").split(",");
       parms.dom.comboBoxWidget.setValue(parms.evalProperty("value"));
       return;
 
@@ -447,25 +445,13 @@ pui.widgets.add({
 
     "choices": function(parms) {
       if (parms.dom.comboBoxWidget != null) {      
-        var choices = parms.value;
-        if (choices != null && choices != "") {
-          parms.dom.comboBoxWidget["choices"] = choices.split(",");
-        }
-        else {
-          parms.dom.comboBoxWidget["choices"] = [];
-        }
+        parms.dom.comboBoxWidget["choices"] = pui.parseCommaSeparatedList(parms.value);
       }
     },
     
     "choice values": function(parms) {
       if (parms.dom.comboBoxWidget != null) {
-        var choiceValues = parms.value;
-        if (choiceValues != null && choiceValues != "") {
-          parms.dom.comboBoxWidget["choice values"] = choiceValues.split(",");
-        }
-        else {
-          parms.dom.comboBoxWidget["choice values"] = [];
-        }
+        parms.dom.comboBoxWidget["choice values"] = pui.parseCommaSeparatedList(parms.value);
       }
     },
     

proddata/js/widgets/menu.js
@@ -464,7 +464,7 @@ pui.widgets.add({
       parms.dom.menuWidget = new pui.MenuWidget();
       var choices = parms.evalProperty("choices");
       if (choices != null && choices != "") {
-        parms.dom.menuWidget.choices = choices.split(",");
+        parms.dom.menuWidget.choices = pui.parseCommaSeparatedList(choices);
       }
       else {
         parms.dom.menuWidget.choices = ["Option 1", "Option 2", "Option 3"];
@@ -493,7 +493,7 @@ pui.widgets.add({
       if (parms.dom.menuWidget != null) {
         var choices = parms.value;
         if (choices != null && choices != "") {
-          parms.dom.menuWidget.choices = choices.split(",");
+          parms.dom.menuWidget.choices = pui.parseCommaSeparatedList(choices);
         }
         else {
           parms.dom.menuWidget.choices = ["Option 1", "Option 2", "Option 3"];
@@ -506,7 +506,7 @@ pui.widgets.add({
       if (parms.dom.menuWidget != null) {
         var choiceValues = parms.value;
         if (choiceValues != null && choiceValues != "") {
-          parms.dom.menuWidget.choiceValues = choiceValues.split(",");
+          parms.dom.menuWidget.choiceValues = pui.parseCommaSeparatedList(choiceValues);
         }
         else {
           parms.dom.menuWidget.choiceValues = [];

proddata/js/widgets/select_box.js
@@ -21,8 +21,10 @@
 
 
 pui.loadSelectBoxChoices = function(choicesString, choiceValuesString, dom) {
-  var choices = choicesString.split(",");
-  var choiceValues = choiceValuesString.split(",");
+
+  var choices = pui.parseCommaSeparatedList(choicesString);
+  var choiceValues = pui.parseCommaSeparatedList(choiceValuesString);
+
   dom.choices = {};
   for (var i = 0; i < choices.length; i++) {
     var optionText = choices[i];

commit 43a1fb6d8a9fe654247d66dd092bc63ae9302c1c

Fix bug introduced in commit 2ab5b32121e6c91365df7e2711e47695832473d2 which caused database-driven auto-complete to stop working due to changes in "choices" and "values" array state when these properties are not specified.

proddata/js/runtime/AutoComplete.js
@@ -861,6 +861,7 @@ function applyAutoComp(properties, originalValue, domObj) {
     var limit = evalPropertyValue(properties["max choices"], originalValue, domObj);
     var choices = evalPropertyValue(properties["choices"], originalValue, domObj);
     choices = pui.parseCommaSeparatedList(choices);
+    if (choices.length == 0) choices = [""];
     var values = evalPropertyValue(properties["choice values"], originalValue, domObj);
     if (values == "") {
       values = [];
@@ -871,6 +872,7 @@ function applyAutoComp(properties, originalValue, domObj) {
     else {
       values = pui.parseCommaSeparatedList(values);
     }
+    if (values.length == 0) values = [""];
 
     // Apply auto complete if any of the settings are given.
     if ((file != "" && fields[0] != "") || url != "" || (choices.length > 0 && choices[0] != "" && values[0] != "")) {