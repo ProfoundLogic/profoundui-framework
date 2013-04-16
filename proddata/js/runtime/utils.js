bcb2af2c749e8fd6374e22e4f3e2da6eb7ce29b4
David Russo <drusso@profoundlogic.com>
2013-04-16 10:05:57 -0400

    (Genie) adjust list of characters which can be entered into 'signed numeric' entry fields.

--- proddata/js/runtime/utils.js
+++ proddata/js/runtime/utils.js
@@ -150,7 +150,7 @@ function numericSignOnly(e){
   if(!e) e = window.event;
   var target = e.srcElement || e.target;
   if (target.autoComp != null) return;	
-  var allowedUnicodes = new Array(8,9,13,16,17,18,19,20,27,33,34,35,36,37,38,39,40,45,46,48,49,50,51,52,53,54,55,56,57,91,93,96,97,98,99,100,101,102,103,104,105,109,110,112,123,144,145,188,190,112,113,114,115,116,117,118,119,120,121,122,123,189,109,pui["field exit key"]);
+  var allowedUnicodes = new Array(8,9,13,16,17,18,19,20,27,33,34,35,36,37,38,39,40,45,46,48,49,50,51,52,53,54,55,56,57,91,93,96,97,98,99,100,101,102,103,104,105,109,112,123,144,145,112,113,114,115,116,117,118,119,120,121,122,123,189,pui["field exit key"]);
   allowKeys(allowedUnicodes, e);
 }
 function alphabeticOnly(e) {
