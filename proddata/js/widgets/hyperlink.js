ProfoundLogic/profoundui-framework

commit 90765c581792b0ea3c2dfcba418ea1b160368e37

Fix hyperlink widget navigating away from the page in IE

proddata/js/widgets/hyperlink.js
@@ -24,12 +24,7 @@ pui["encode hyperlink spaces"] = null;
 pui.buildHyperlink = function(dom, value, designMode, href, target) {
   dom.innerHTML = "";
   var a = document.createElement("a");
-  if (designMode == true || href == null) {
-    href = "javascript:void(0)";
-    if (is_ie && designMode == true) {
-      href = "#";
-    }
-  }
+  if (designMode == true || href == null) href = "javascript:void(0)";
   a.href = href;
   if (!designMode && target != null) a.target = target;
   var text = value;
@@ -42,12 +37,9 @@ pui.buildHyperlink = function(dom, value, designMode, href, target) {
   a.appendChild(document.createTextNode(text));
   if (is_ie && (context == "dspf" || context == "genie")) {
     addEvent(a, "click", function(e) {
+      e.preventDefault();
       e.returnValue = false;
       return false;      
-      //pui.skipConfirm = true;
-      //setTimeout(function() {
-      //  pui.skipConfirm = false;
-      //}, 0);
     });
   }
   dom.appendChild(a);

commit 6c3613590023d76dd3bc539b69c92599999054aa

Corrected bug introduced in commit 90765c581792b0ea3c2dfcba418ea1b160368e37 that caused call to undefined method "event.preventDefault" when clicking a link in IE versions prior to 9.

proddata/js/widgets/hyperlink.js
@@ -37,7 +37,7 @@ pui.buildHyperlink = function(dom, value, designMode, href, target) {
   a.appendChild(document.createTextNode(text));
   if (is_ie && (context == "dspf" || context == "genie")) {
     addEvent(a, "click", function(e) {
-      e.preventDefault();
+      if (e.preventDefault) e.preventDefault(); 
       e.returnValue = false;
       return false;      
     });
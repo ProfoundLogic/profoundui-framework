6c3613590023d76dd3bc539b69c92599999054aa
David Russo <drusso@profoundlogic.com>
2013-04-29 16:10:53 -0400

    Corrected bug introduced in commit 90765c581792b0ea3c2dfcba418ea1b160368e37 that caused call to undefined method "event.preventDefault" when clicking a link in IE versions prior to 9.

--- proddata/js/widgets/hyperlink.js
+++ proddata/js/widgets/hyperlink.js
@@ -37,7 +37,7 @@ pui.buildHyperlink = function(dom, value, designMode, href, target) {
   a.appendChild(document.createTextNode(text));
   if (is_ie && (context == "dspf" || context == "genie")) {
     addEvent(a, "click", function(e) {
-      e.preventDefault();
+      if (e.preventDefault) e.preventDefault(); 
       e.returnValue = false;
       return false;      
     });
