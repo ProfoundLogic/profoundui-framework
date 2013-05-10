ProfoundLogic/profoundui-framework

commit 90765c581792b0ea3c2dfcba418ea1b160368e37

Fix hyperlink widget navigating away from the page in IE

proddata/js/widgets/hyperlink.js
@@ -24,12 +24,7 @@ pui["encode hyperlink spaces"] = null;
 pui.buildHyperlink = function(dom, value, designMode, href, target) {
   dom.innerHTML = "";
   var a = document.createElement("a");
-  if (designMode == true || href == null) {
-    href = "javascript:void(0)";
-    if (is_ie && designMode == true) {
-      href = "#";
-    }
-  }
+  if (designMode == true || href == null) href = "javascript:void(0)";
   a.href = href;
   if (!designMode && target != null) a.target = target;
   var text = value;
@@ -42,12 +37,9 @@ pui.buildHyperlink = function(dom, value, designMode, href, target) {
   a.appendChild(document.createTextNode(text));
   if (is_ie && (context == "dspf" || context == "genie")) {
     addEvent(a, "click", function(e) {
+      e.preventDefault();
       e.returnValue = false;
       return false;      
-      //pui.skipConfirm = true;
-      //setTimeout(function() {
-      //  pui.skipConfirm = false;
-      //}, 0);
     });
   }
   dom.appendChild(a);

commit 618421607af2ca9484b39ebd9946017c98babf85

Corrected bug introduced in commit 90765c581792b0ea3c2dfcba418ea1b160368e37 which prevented hyperlink href action in Internet Explorer.

proddata/js/widgets/hyperlink.js
@@ -24,7 +24,11 @@ pui["encode hyperlink spaces"] = null;
 pui.buildHyperlink = function(dom, value, designMode, href, target) {
   dom.innerHTML = "";
   var a = document.createElement("a");
-  if (designMode == true || href == null) href = "javascript:void(0)";
+  var noHref = false;
+  if (designMode == true || href == null)  { 
+    href = "javascript:void(0)";
+    noHref = true;
+  }
   a.href = href;
   if (!designMode && target != null) a.target = target;
   var text = value;
@@ -35,7 +39,7 @@ pui.buildHyperlink = function(dom, value, designMode, href, target) {
   if (dom.style.color != null) a.style.color = dom.style.color;
   if (dom.style.textDecoration != null) a.style.textDecoration = dom.style.textDecoration;
   a.appendChild(document.createTextNode(text));
-  if (is_ie && (context == "dspf" || context == "genie")) {
+  if (is_ie && noHref && (context == "dspf" || context == "genie")) {
     addEvent(a, "click", function(e) {
       if (e.preventDefault) e.preventDefault(); 
       e.returnValue = false;