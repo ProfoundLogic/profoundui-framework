ProfoundLogic/profoundui-framework

commit c6dbc77d75bcfb923e77b9431831f35c961cf74b

Fixup problem where pulldown menu's submenus disappear in IE8 before you can click them

proddata/js/widgets/menu.js
@@ -184,6 +184,16 @@ pui.MenuWidget = function() {
           if (!e) e = window.event;
           var tgt = e.relatedTarget;
           if (tgt == null) tgt = e.toElement;
+          
+          // Some browsers (IE8) seem to set the "toElement" to the <TABLE>
+          // instead of the <TD> that this routine expects.  This finds the <TD>...
+          while ( tgt.tagName == "TABLE" 
+                 || tgt.tagName == "TBODY" 
+                 || tgt.tagName == "TR") {
+            if (tgt.firstChild == null) break;
+            tgt = tgt.firstChild;
+          }
+          
           if (tgt == null || tgt.level == null || tgt.tagName != "TD" || tgt.menuId != me.container.id) {
             var keep = false;
             if (tgt != null) {

commit 2079667aeae46848af168e6a525db7fb75e8fced

Fixed issue introduced in c6dbc77 when moving mouse outside of window. relatedTarget is null, so added check for null.

proddata/js/widgets/menu.js
@@ -187,9 +187,9 @@ pui.MenuWidget = function() {
           
           // Some browsers (IE8) seem to set the "toElement" to the <TABLE>
           // instead of the <TD> that this routine expects.  This finds the <TD>...
-          while ( tgt.tagName == "TABLE" 
+          while (tgt!=null && ( tgt.tagName == "TABLE" 
                  || tgt.tagName == "TBODY" 
-                 || tgt.tagName == "TR") {
+                 || tgt.tagName == "TR")) {
             if (tgt.firstChild == null) break;
             tgt = tgt.firstChild;
           }