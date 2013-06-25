14de8fd1db29803b31f5738b925db4985b5d640d
David Russo <drusso@profoundlogic.com>
2013-06-25 10:45:55 -0400

    Added new date locale "he_IL" (Hebrew, Israel).

---
+++ proddata/js/runtime/locales/he_IL.js
@@ -0,0 +1,124 @@
+﻿//  Profound UI Runtime  -- A Javascript Framework for Rich Displays
+//  Copyright (c) 2013 Profound Logic Software, Inc.
+//
+//  This file is part of the Profound UI Runtime
+//
+//  The Profound UI Runtime is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  The Profound UI Runtime is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  In the COPYING and COPYING.LESSER files included with the Profound UI Runtime.
+//  If not, see <http://www.gnu.org/licenses/>.
+
+
+pui.locales.add({
+  'name': "he_IL",
+  'text': "Hebrew (Israel)",
+  'datePatterns': [
+    'Y-m-d',
+    'm/d/y',
+    'm/d/Y',
+    'n/j/y',
+    'n/j/Y',
+    'm-d-y',
+    'm-d-Y',
+	  'n-j-y',
+	  'n-j-Y',
+	  'Y/m',
+	  'm/Y',
+	  'Y/m/d',
+	  'y/m/d',
+	  'l, F j, Y',
+    'l, F jS, Y',
+    'D, M d, y',
+    'D, M d, Y',
+    'D M d y',
+    'D M d Y',
+    'F j, y',
+    'F j, Y',
+    'M j, y',
+    'M j, Y',
+    'j-M-y',
+    'j-M-Y'
+  ],
+  'timePatterns': [
+    'g:i',
+    'g:i:s',
+	  'g:i a',
+    'g:i A',
+    'H:i',
+    'H.i.s',
+    'H:i:s',
+	  'H:i a',
+	  'H:i A',
+    'g:i:s a',
+    'g:i:s A',
+    'H:i:s a',
+    'H:i:s A'
+  ],
+  'timeStampPatterns': [
+    'm/d/y g:i A',
+    'm/d/y g:i a',
+    'm/d/y H:i:s',
+    'm-d-y g:i A',
+    'm-d-y g:i a',
+    'm-d-y H:i:s',
+    'D, M d, Y H:i:s',
+    'g:i A o\\n n/j/y',
+    'Y-m-d-H.i.s.uu'
+  ],
+  'monthNames': [
+    'ינואר',
+    'פברואר',
+    'מארס',
+    'אפריל',
+    'מאי',
+    'יוני',
+    'יולי',
+    'אוגוסט',
+    'ספטמבר',
+    'אוקטובר',
+    'נובמבר',
+    'דצמבר'
+  ],
+  'shortMonthNames': [
+    '.ינ',
+    'פבר',
+    'מארס',
+    'אפר',
+    'מאי',
+    'יוני',
+    'יולי',
+    'אוג',
+    'ספט',
+    'אוק',
+    'נוב',
+    'דצמ'
+  ],
+  'dayNames': [
+    'ראשון',
+    'שני',
+    'שלישי',
+    'רביעי',
+    'חמישי',
+    'שישי',
+    'שבת'
+  ],
+  'shortDayNames': [
+    'א',
+    'ב',
+    'ג',
+    'ד',
+    'ה',
+    'ו',
+    'ש'
+  ],
+  'weekStart': 0
+});
