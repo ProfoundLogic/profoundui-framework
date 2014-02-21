404ebbb24f36e21115f09bf177e84ea4d4aa34a5
Scott Klement <sklement@profoundlogic.com>
2014-02-21 14:12:21 -0500

    Add config option pui["use original edit mask"] so that the original edit mask code (that keeps all zeros, irrespective of the edit word) is used instead of the fixed version

--- proddata/js/runtime/dspf/editWord.js
+++ proddata/js/runtime/dspf/editWord.js
@@ -220,7 +220,13 @@ pui.applyEditWord = function(obj) {
 }
 
 
-pui.applyEditMask = function(dom, event, pos) {
+// -------------------------------------------------------------------
+//  This is the original applyEditMask() function.  It does not follow
+//  IBM's specs, but we are keeping it to allow backward compatibility
+//  via the pui["use original edit mask"] config option.
+// -------------------------------------------------------------------
+
+pui.applyEditMaskOriginal = function(dom, event, pos) {
   // get key code
   event = event || window.event;
   var key;
@@ -242,7 +248,98 @@ pui.applyEditMask = function(dom, event, pos) {
   var value = dom.value;
   if (value == null) return;
   
-  // derrive mask character array
+  // derive mask character array
+  var mask = [];
+  var n = edtMsk.length;
+  if (edtWrd.length < n) n = edtWrd.length;
+  for (var i = 0; i < n; i++) {
+    wrdCh = edtWrd.substr(i, 1);
+    mskCh = edtMsk.substr(i, 1);
+    if (wrdCh == " " || wrdCh == "0" || mskCh == " ") {
+      mask.push(null);
+    }
+    else {
+      if (wrdCh == "&") mask.push(" ");
+      else mask.push(wrdCh);
+    }
+  }
+  
+  // apply mask
+  n = mask.length;
+  while (value.length < n) {
+    if (key == null || key == 0) value = " " + value;
+    else value += " ";
+  }
+  if (value.length > n) {
+    value = value.substr(0, n);
+  }
+  var fillCh = "0";
+  for (var i = 0; i < n; i++) {
+    var maskCh = mask[i];
+    var valueCh = value.substr(i, 1);
+    if (maskCh != null && valueCh != maskCh) {
+      value = value.substr(0, i) + maskCh + value.substr(i+1);
+    }
+    else if (maskCh == null && (valueCh < "0" || valueCh > "9")) {
+      value = value.substr(0, i) + fillCh + value.substr(i+1);
+    }
+  }
+  value = rtrim(value);
+  
+  if (value != rtrim(dom.value) || pos != null) {
+    if (pos == null) pos = getCursorPosition(dom);
+    
+    if (key != 8) {  // not backspace
+      while (pos < mask.length && mask[pos] != null) pos++;
+    }    
+    
+    dom.value = value;
+    
+    // position cursor
+    if (dom.createTextRange != null && !pui["is_opera"]) { 
+      var tr = dom.createTextRange(); 
+      if (tr) { 
+        tr.moveStart("character", pos);
+        tr.collapse(); 
+        tr.select(); 
+      }
+    }
+    else if (dom.selectionStart != null) {  // Firefox
+      dom.setSelectionRange(pos, pos);
+    }
+  }
+}
+
+
+pui.applyEditMask = function(dom, event, pos) {
+	
+  // for backward compatibility:
+  if ( typeof pui["use original edit mask"] != "undefined" 
+     && pui["use original edit mask"] === true ) {
+    return pui.applyEditMaskOriginal(dom, event, pos);
+  }
+  // get key code
+  event = event || window.event;
+  var key;
+  if (event != null) key = event.keyCode;   
+
+  // get edit word, edit mask, and value
+  var formatting = dom.formattingInfo;
+  if (formatting == null) return;
+  var edtMsk = formatting.edtMsk;
+  var edtWrd = formatting.edtWrd;
+  if (edtMsk == null || edtMsk == "") return;
+  if (edtWrd == null || edtWrd == "") return;  // for now, edit masks only work with edit words
+  if (edtWrd.length > 1 && edtWrd.substr(0, 1) == "'" && edtWrd.substr(edtWrd.length-1, 1) == "'") {
+    edtWrd = edtWrd.substr(1, edtWrd.length - 2);
+  }
+  if (edtMsk.length > 1 && edtMsk.substr(0, 1) == "'" && edtMsk.substr(edtMsk.length-1, 1) == "'") {
+    edtMsk = edtMsk.substr(1, edtMsk.length - 2);
+  }
+  var value = dom.value;
+  if (value == null) return;
+  
+  // derive mask character array
   var mask = [];
   var n = edtMsk.length;
   var zeroSuppressPos = n;
@@ -308,9 +405,10 @@ pui.applyEditMask = function(dom, event, pos) {
         tr.select(); 
       }
     }
-    else if (dom.selectionStart != null) {  // Firefox
+    else if (dom.selectionStart != null) {  // Firefox 
       dom.setSelectionRange(pos, pos);
     }
   }
 }
+
 
