ProfoundLogic/profoundui-framework

commit 89cf96264a59c8e7d1980becd67eb0a6058c7a42

Escape reserved characters in chart XML data when "names" and "values" are used.

proddata/js/runtime/utils.js
@@ -1558,3 +1558,14 @@ pui.fixCheckPrint = function(el) {
   }
 
 }
+
+pui.xmlEscape = function(str) {
+
+  str = "" + str;
+  str = str.replace(/</g, "&lt;");
+  str = str.replace(/>/g, "&gt;");
+  str = str.replace(/&/g, "&amp;");
+  str = str.replace(/"/g, "&quot;");
+  return str;
+
+}

proddata/js/widgets/chart.js
@@ -342,7 +342,6 @@ pui.widgets.add({
                 var retrievedDataName = get(dataName);
                 if (retrievedDataName != "") dataName = retrievedDataName;
               }
-              dataName = dataName.replace(/"/g, "'");  // '
               var dataValue = valueArray[i];
               if (dataValue == null) dataValue = "0";
               dataValue = String(dataValue);
@@ -359,7 +358,7 @@ pui.widgets.add({
                 dataValue = Number(dataValue);
               }
               if (isNaN(dataValue)) dataValue = 0;
-              chartXML += '<set name="' + dataName + '" value="' + dataValue + '" />';
+              chartXML += '<set name="' + pui.xmlEscape(dataName) + '" value="' + pui.xmlEscape(dataValue) + '" />';
             }
             chartXML += '</chart>';
             

commit c7d97bd19870561c27c77eddb8efde0b9cd1bab7

Fix bug in XML escape code introduced in commit 89cf96264a59c8e7d1980becd67eb0a6058c7a42.

proddata/js/runtime/utils.js
@@ -1562,9 +1562,9 @@ pui.fixCheckPrint = function(el) {
 pui.xmlEscape = function(str) {
 
   str = "" + str;
+  str = str.replace(/&/g, "&amp;");
   str = str.replace(/</g, "&lt;");
   str = str.replace(/>/g, "&gt;");
-  str = str.replace(/&/g, "&amp;");
   str = str.replace(/"/g, "&quot;");
   return str;
 