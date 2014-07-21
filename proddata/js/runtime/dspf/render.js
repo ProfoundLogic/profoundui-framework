ProfoundLogic/profoundui-framework

commit 5e21ae6cbbecfa2e13341de4c424cd82c586aff9

Fix issue of positioning the cursor onto an input element within a grid in combination with the subfile record number property

proddata/js/runtime/dspf/render.js
@@ -791,6 +791,15 @@ pui.render = function(parms) {
   //}
 
   pui.screenIsReady = true;
+  
+  setTimeout(function() {
+    for (var i = 0; i < pui.gridsDisplayed.length; i++) {
+      var grid = pui.gridsDisplayed[i];
+      if (grid.scrollbarObj != null) {
+        grid.scrollbarObj.ready = true;
+      }
+    }
+  }, 0);
 
 }
 
@@ -2222,7 +2231,7 @@ pui.renderFormat = function(parms) {
       }
     }
     if (grid.scrollbarObj != null) {
-      grid.scrollbarObj.ready = true;
+      //grid.scrollbarObj.ready = true; ... this now happens at the end of pui.render() instead of here, since we don't want to cause the onscroll event to fire prematurely
       if (!isDesignMode && grid.scrollbarObj.type == "sliding" && grid.scrollbarObj.rowsPerPage == 1) {
         grid.scrollbarObj.draw();
       }

commit 2af358bff3cb13a65a5b0c1f61e958199fa50dce

Fix cursor positioning with 'subfile record number' and 'place cursor' when using a sliding scrollbar.  This was inadvertantly broken in commit 5e21ae6cbbecfa2e13341de4c424cd82c586aff9

proddata/js/runtime/dspf/render.js
@@ -2203,13 +2203,11 @@ pui.renderFormat = function(parms) {
     if (grid.recNum > 1 && grid.scrollbarObj != null && grid.scrollbarObj.type == "sliding") {
       grid.scrollbarObj.setScrollTopToRow(grid.recNum, true);
     }
-    else {
-      if (pui.placeCursorOnSubfile && pui.cursorValues.setColumn == null && pui.cursorValues.setRow == null) {
-        if (pui.focusField == null || pui.focusField.setFocusFlag != true) {
-          setTimeout(function() {
-            grid.placeCursor(true);
-          }, 1);
-        }
+    if (pui.placeCursorOnSubfile && pui.cursorValues.setColumn == null && pui.cursorValues.setRow == null) {
+      if (pui.focusField == null || pui.focusField.setFocusFlag != true) {
+        setTimeout(function() {
+          grid.placeCursor(true);
+        }, 1);
       }
     }
     if (grid.scrollbarObj != null) {