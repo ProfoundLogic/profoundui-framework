fb2fa087128e8c86139c892af25800a2a5fb8c3f
Scott Klement <sklement@profoundlogic.com>
2015-01-22 18:09:25 -0500

    Update copyright statements to 2015

--- proddata/js/runtime/dspf/mobileEmulator.js
+++ proddata/js/runtime/dspf/mobileEmulator.js
@@ -1,6 +1,6 @@
 
 //  Profound UI Runtime  -- A Javascript Framework for Rich Displays
-//  Copyright (c) 2014 Profound Logic Software, Inc.
+//  Copyright (c) 2015 Profound Logic Software, Inc.
 //
 //  This file is part of the Profound UI Runtime
 //
