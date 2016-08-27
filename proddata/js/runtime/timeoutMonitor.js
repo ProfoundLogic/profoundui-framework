3a9a3c775b149f495a10748c2c31a0d9754e7e44
Scott Klement <sklement@profoundlogic.com>
2016-08-26 20:38:41 -0400

    Added copyright statements to files that were missing them

--- proddata/js/runtime/timeoutMonitor.js
+++ proddata/js/runtime/timeoutMonitor.js
@@ -1,3 +1,23 @@
+//  Profound UI Runtime  -- A Javascript Framework for Rich Displays
+//  Copyright (c) 2016 Profound Logic Software, Inc.
+//
+//  This file is part of the Profound UI Runtime
+//
+//  The Profound UI Runtime is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  The Profound UI Runtime is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  In the COPYING and COPYING.LESSER files included with the Profound UI Runtime.
+//  If not, see <http://www.gnu.org/licenses/>.
+
+
 // pui.timeoutMonitor.timer.reset() called in the onuseractivity function
 // pui.timeoutMonitor.start() called in the rendering functions in both Rich Displays and Genie
 // pui.timeoutMonitor.end() called when a screen is submitted
