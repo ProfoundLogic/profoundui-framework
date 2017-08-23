f345f4dce686d04f9dad1ee5df59721645d3507d
Darpan Patel <dpatel@profoundlogic.com>
2017-08-23 17:31:29 -0400

    Negative Sign appears when editword is used. Negative Number formatting #3780 and #3397. Resolved.

--- proddata/js/runtime/dspf/formatting.js
+++ proddata/js/runtime/dspf/formatting.js
@@ -585,12 +585,15 @@ pui.FieldFormat = {
       
       var value = String(obj.value);
       
-      if (obj.edtWrd != null && obj.edtWrd != null) {
+      if (obj.edtWrd != null) {
+        var negSign = "-";
+        var g1 = /-/.test(value);
+        if(!g1) negSign = "";
         var originalValue = value;
         value = "";
         for (var i = 0; i < originalValue.length; i++) {
           var ch = originalValue.substr(i,1);
-          if ((ch < "0" || ch > "9") && (ch != decimalChar)) {
+          if ((ch < "0" || ch > "9") && (ch != decimalChar) && (ch != negSign)) {     // this enables the negative sign appear when editWord is applied with the sign "-".
             ch = "";
           }
           value += ch;
