0fe44dbbac2b32d4012cb1292d5da558810ef99d
Jeff Berman <jberman@profoundlogic.com>
2018-02-20 15:18:37 -0500

    Wait for any existing PUI callbacks to complete before rendering new screen. #4126

--- proddata/js/runtime/dspf/api.js
+++ proddata/js/runtime/dspf/api.js
@@ -239,19 +239,24 @@ pui["show"] = function(parms) {
     success: true
   }
   
-  if (container == null) {
-    pui.handler = handler;
-    if (pui.handler == null) pui.handler = function() { };
-    pui.render(obj);
-  }
-  else {
-    container.innerHTML = "";
-    var format = layers[0]["formats"][0];
-    format.lastLayer = true;
-    format.lastFormat = true;
-    format.container = container;
-    pui.renderFormat(format);
-  }
+  // Render screen after waiting for any existing PUI callbacks to complete, so
+  // objects they reference aren't destroyed prematurely.
+  setTimeout(function () {
+    if (container == null) {
+      pui.handler = handler;
+      if (pui.handler == null) pui.handler = function() { };
+      pui.render(obj);
+    }
+    else {
+      container.innerHTML = "";
+      var format = layers[0]["formats"][0];
+      format.lastLayer = true;
+      format.lastFormat = true;
+      format.container = container;
+      pui.renderFormat(format);
+    }
+  }, 0);
+
 
 }
 
