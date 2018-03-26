ProfoundLogic/profoundui-framework

commit b0f51248296657f19edd23e5c2cff42b179244dc

Add "transition animation" property

proddata/js/runtime/dspf/render.js
@@ -802,7 +802,12 @@ pui.render = function(parms) {
       pui.lastWindow = null;
       pui.lastWindowLeft = null;
       pui.lastWindowTop = null;
-      parms.container.innerHTML = "";
+      parms.container.innerHTML = "";      
+      var animation = formats[0]["metaData"]["screen"]["transition animation"];
+      if (pui.canvasSize) animation = null;
+      if (animation) {
+        parms.container = pui.transitionAnimation.setup(parms.container, animation);
+      }
     }
     else {
       pui.setupWindowDiv(parms, layers[i]);
@@ -860,6 +865,8 @@ pui.render = function(parms) {
     pui.windowStack.push(parms.container);
     
   }
+  
+  if (animation) pui.transitionAnimation.animate();
 
   if (pui.focusField != null && pui.focusField.dom != null && (!pui.placeCursorOnSubfile || pui.cursorValues.setRow != null || pui.cursorValues.setColumn != null || pui.focusField.setFocusFlag == true) ) {
     var cell = pui.focusField.dom.parentNode;
@@ -5822,3 +5829,47 @@ pui.isInputCapableProperty = function(propname, dom) {
 
   return false;
 };
+
+
+pui.transitionAnimation = {
+  setup: function(container, animation) {
+    this.animation = animation;
+    
+    // Capture previous screen and remove any id's to avoid clashing when the new screen is rendered
+    var containerId = container.id;
+    this.prevScreen = container;
+    this.prevScreen.id = "pui-previous";
+    this.prevScreen.style.zIndex = 10;
+    var allElems = this.prevScreen.getElementsByTagName("*");
+    for (var i = 0; i < allElems.length; i++) {
+      var elem = allElems[i];
+      if (elem.hasAttribute("id")) elem.removeAttribute("id");
+    }
+
+    // Create new screen as brand new pui div
+    this.newScreen = document.createElement("div");
+    this.newScreen.id = containerId;
+    this.newScreen.style.zIndex = 20;
+    this.prevScreen.parentNode.appendChild(this.newScreen);
+    return this.newScreen;
+  },
+  
+  animate: function() {
+    var animatedScreen = this.newScreen;
+    if (this.animation === "slide-out") {  // certain types of animation are intended to remove the previous screen rather than introduce the new screen on top of the previous screen
+      this.prevScreen.style.zIndex = 30;
+      animatedScreen = this.prevScreen;
+    }
+    animatedScreen.addEventListener("animationend", function(event) {
+      pui.transitionAnimation.cleanup();
+    }, false);
+    animatedScreen.className = this.animation;
+  },
+  
+  cleanup: function() {
+    if (!this.prevScreen) return;
+    this.prevScreen.innerHTML = "";
+    if (this.prevScreen.parentNode) this.prevScreen.parentNode.removeChild(this.prevScreen);
+    this.prevScreen = null;
+  }
+}

proddata/js/runtime/properties.js
@@ -412,6 +412,7 @@ function getScreenPropertiesModel(designScreen) {
     	{ name: "design overlay formats", type: "list", help: "Specifies a list of additional record formats to render in the designer when this record format is selected.  This property is only used at design-time.  It is ignored at run-time.", bind: false },
 
     	{ name: "Behavior", category: true },
+      { name: "transition animation", help: "This property identifies the CSS class for the screen transition animation." },
     	{ name: "disable enter key", choices: ["true", "false"], type: "boolean", help: "This property determines if pressing the Enter key will cause a response to be sent to the server.  If set to true and the Enter key is not used as a shortcut key, the response will not be sent.  Otherwise, the response is sent automatically.", hideFormatting: true, validDataTypes: ["indicator", "expression"] },
     	{ name: "initialize record", choices: ["true", "false"], type: "boolean", bind: false, help: "Specifies that if this record is not already on the display, it is to be written to the display before an input operation is sent from the program.  It represents the INZRCD keyword.", viewdesigner: false },
     	{ name: "protect", choices: ["true", "false"], type: "boolean", help: "Specifies that when this record is displayed, all input-capable fields already on the display become protected.  The read only property is set to true and the PR css class is applied.", hideFormatting: true, validDataTypes: ["indicator", "expression"] },

commit bdae2ac383282b3083a28081c10202385dae3aef

Fixed render error when screen metaData doesn't exist. Caused by b0f51248. Issue 4315.

proddata/js/runtime/dspf/render.js
@@ -802,7 +802,10 @@ pui.render = function(parms) {
       pui.lastWindow = null;
       pui.lastWindowLeft = null;
       pui.lastWindowTop = null;
-      var animation = pui.evalBoundProperty(formats[0].metaData.screen["animation"], formats[0].data, formats[0].ref);
+      var animation = null;
+      if (formats.length > 0 && formats[0].metaData != null && formats[0].metaData.screen != null ){
+        animation = pui.evalBoundProperty(formats[0].metaData.screen["animation"], formats[0].data, formats[0].ref);
+      }
       if (parms.transition && parms.transition["animation"]) {
         animation = parms.transition["animation"];
       }
@@ -818,7 +821,7 @@ pui.render = function(parms) {
           animation: animation,
           animatedScreenProperty: pui.evalBoundProperty(formats[0].metaData.screen["animated screen"], formats[0].data, formats[0].ref),
           overlay: pui.evalBoundProperty(formats[0].metaData.screen["overlay screens"], formats[0].data, formats[0].ref)
-        }
+        };
         if (parms.transition && parms.transition["animation"]) {
           setupObj.animatedScreenProperty = parms.transition["screen"];
           setupObj.overlay = parms.transition["overlay"];