b487f43b973b37c4560fdbd7acb8ee1359c39999
Niral Pokal <npokal@profoundlogic.com>
2018-10-08 12:14:38 -0400

    Update ProfoundUI to version to 6.1.2

--- proddata/js/runtime/version.js
+++ proddata/js/runtime/version.js
@@ -22,5 +22,5 @@
 
 if (!window["pui"]) window["pui"] = {};
 window["pui"]["baseVersion"] = '6';
-window["pui"]["fixPackVersion"] = '1.1';
+window["pui"]["fixPackVersion"] = '1.2';
 window["pui"]["version"] = window["pui"]["baseVersion"] + "." + window["pui"]["fixPackVersion"];
