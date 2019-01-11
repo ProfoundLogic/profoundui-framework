ProfoundLogic/profoundui-framework

commit 223db76e5e990be2050a935ede9e8bdcc02fcfa6

Render text fields with autocomplete="off" Add "html autocomplete" property

proddata/js/runtime/properties.js
@@ -578,6 +578,7 @@ function getPropertiesModel() {
     { name: "empty text", controls: ["combo box", "date field", "spinner", "text area", "textbox"], help: pui.helpTextProperties("blank", "Specifies the default text to place into an empty field. When the field receives focus, the text is removed. This property is similar to the 'placeholder' property, but provides support for older browser that may not yet support the placeholder HTML5 attribute."), translate: true },
     { name: "placeholder", attribute: "placeholder", controls: ["combo box", "date field", "spinner", "text area", "textbox", "password field"], help: pui.helpTextProperties("blank", "Uses the HTML5 placeholder attribute to specify a short hint that describes the expected value of an input field. Older browsers may not support this feature."), translate: true },
     { name: "input type", choices: ["color", "date", "datetime", "datetime-local", "email", "month", "number", "range", "search", "tel", "time", "url", "week"], controls: ["combo box", "date field", "textbox"], help: pui.helpTextProperties("textbox", "Specifies an HTML5 input type. Some types may not yet be supported by the user's browser or mobile device. If a type is not specified or if the selected type is not supported, a standard textbox element will be used.") },
+    { name: "html autocomplete", hideFormatting: true, choices: ["on", "off", "Other..."], controls: ["combo box", "date field", "password field", "textbox", "spinner"], help: pui.helpTextProperties("off", "Specifies the value of the HTML textbox \"autocomplete\" attribute, which controls the browser's autocomplete/autofill feature. Browser autocomplete/autofill is disabled (\"off\") by default. Specify \"on\" to enable browser autocomplete/autofill or for further control, specify an autofill field name. See <a href=\"https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill-field\" target=\"_blank\">here</a> for details on autofill field names.") },
     { name: "related field", help: pui.helpTextProperties("blank", "This property allows you to create a radio button group by associating multiple radio buttons with a field from the original application. Specify the id of the field to associate the radio button with. Additionally, this property can associate a text area with a group of textboxes by specify a comma separated list of textbox id's."), controls: ["radio button", "text area"], context: "genie" },
     { name: "select box height", format: "number", attribute: "size", choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "Other..."], help: pui.helpTextProperties("5", "If specified, the select box appears as a list box; if omitted, the select box appears as a dropdown.", ["other"]), controls: ["select box"] },
     { name: "multiple", attribute: "multiple", choices: ["true", "false"], type: "boolean", help: pui.helpTextProperties("false", "Specifies that multiple options can be selected at once in a List Box. The options are returned as a comma separated list."), controls: ["select box"], context: "dspf", hideFormatting: true, validDataTypes: ["indicator", "expression"] },

proddata/js/widgets/combo_box.js
@@ -72,7 +72,8 @@ pui.ComboBoxWidget = function() {
     }
 
     box.type = "text";
-    box.setAttribute("autocomplete", "off");
+    if (box.getAttribute("autocomplete") == null) // Default off if not already set by 'html auto complete' property.
+      box.setAttribute("autocomplete", "off");
     box.style.position = "absolute";
     box.style.border = "0px none";
     box.style.top = "0px";
@@ -666,6 +667,11 @@ pui.widgets.add({
           parms.dom.comboBoxWidget.getBox().setAttribute("type", parms.value);
       }
       catch(e) { }
+    },
+    
+    "html autocomplete": function(parms) {
+      if (!parms.design && parms.dom.comboBoxWidget != null)
+        parms.dom.comboBoxWidget.getBox().setAttribute("autocomplete", parms.value);
     }
 
   },

proddata/js/widgets/date_field.js
@@ -598,6 +598,8 @@ pui.widgets.add({
             }
             
           }, 1);
+          if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+            parms.dom.setAttribute("autocomplete", "off");
         }
         if (pui.iPadEmulation && !pui.iPhoneEmulation) {
           addEvent(parms.dom, "focus", function(event) {
@@ -668,6 +670,10 @@ pui.widgets.add({
           parms.dom["cal icon class"] = className;
         }
       }
+    },
+    "html autocomplete": function(parms) {
+      if (!parms.design)
+        parms.dom.setAttribute("autocomplete", parms.value);
     }
   }
   

proddata/js/widgets/password_field.js
@@ -29,18 +29,27 @@ pui.widgets.add({
   
     "field type": function(parms) {
       parms.dom.value = parms.evalProperty("value");
-      if (!parms.design && pui.iPadEmulation && !pui.iPhoneEmulation) {
-        addEvent(parms.dom, "focus", function(event) {
-          getObj("ipadKeyboard").style.display = "";
-        });
-        addEvent(parms.dom, "blur", function(event) {
-          getObj("ipadKeyboard").style.display = "none";
-        });
+      if (!parms.design) {
+        if (pui.iPadEmulation && !pui.iPhoneEmulation) {
+          addEvent(parms.dom, "focus", function(event) {
+            getObj("ipadKeyboard").style.display = "";
+          });
+          addEvent(parms.dom, "blur", function(event) {
+            getObj("ipadKeyboard").style.display = "none";
+          });
+        }
+        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+          parms.dom.setAttribute("autocomplete", "off");
       }
     },
     
     "value": function(parms) {
       parms.dom.value = parms.value;
+    },
+    
+    "html autocomplete": function(parms) {
+      if (!parms.design)
+        parms.dom.setAttribute("autocomplete", parms.value);
     }
   
   }

proddata/js/widgets/spinner.js
@@ -236,6 +236,8 @@ pui.widgets.add({
             parms.dom.spinner.positionSpinnButtons();
           }
         }, 1);
+        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+          parms.dom.setAttribute("autocomplete", "off");
       }
       else {
         parms.dom.sizeMe = function() {
@@ -287,6 +289,11 @@ pui.widgets.add({
         up.className = upClass;
         down.className = downClass; 
       }
+    },
+    
+    "html autocomplete": function(parms) {
+      if (!parms.design)
+        parms.dom.setAttribute("autocomplete", parms.value);
     }
 
   }

proddata/js/widgets/textbox.js
@@ -46,6 +46,8 @@ pui.widgets.add({
           parms.dom.spellcheck = pui["allow spellcheck"];
         //If they are changing a date field to a textbox, remove the calendar
         if(parms.dom.calimg) pui.removeCal(parms.dom);
+        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+          parms.dom.setAttribute("autocomplete", "off");
       }
       if (parms.design) { 
         parms.dom.readOnly = true;
@@ -72,6 +74,11 @@ pui.widgets.add({
           parms.dom.setAttribute("type", parms.value);
         } catch(e) { }
       }
+    },
+    
+    "html autocomplete": function(parms) {
+      if (!parms.design)
+        parms.dom.setAttribute("autocomplete", parms.value);
     }
   
   }

commit 6a8252965b855111431c8f66410a12de1a47c1da

Fix Genie "browser auto complete" setting, broken by 223db76e5e990be2050a935ede9e8bdcc02fcfa6 Rename "html autocomplete" property to "browser auto complete" for consistency

proddata/js/runtime/properties.js
@@ -578,7 +578,7 @@ function getPropertiesModel() {
     { name: "empty text", controls: ["combo box", "date field", "spinner", "text area", "textbox"], help: pui.helpTextProperties("blank", "Specifies the default text to place into an empty field. When the field receives focus, the text is removed. This property is similar to the 'placeholder' property, but provides support for older browser that may not yet support the placeholder HTML5 attribute."), translate: true },
     { name: "placeholder", attribute: "placeholder", controls: ["combo box", "date field", "spinner", "text area", "textbox", "password field"], help: pui.helpTextProperties("blank", "Uses the HTML5 placeholder attribute to specify a short hint that describes the expected value of an input field. Older browsers may not support this feature."), translate: true },
     { name: "input type", choices: ["color", "date", "datetime", "datetime-local", "email", "month", "number", "range", "search", "tel", "time", "url", "week"], controls: ["combo box", "date field", "textbox"], help: pui.helpTextProperties("textbox", "Specifies an HTML5 input type. Some types may not yet be supported by the user's browser or mobile device. If a type is not specified or if the selected type is not supported, a standard textbox element will be used.") },
-    { name: "html autocomplete", hideFormatting: true, choices: ["on", "off", "Other..."], controls: ["combo box", "date field", "password field", "textbox", "spinner"], help: pui.helpTextProperties("off", "Specifies the value of the HTML textbox \"autocomplete\" attribute, which controls the browser's autocomplete/autofill feature. Browser autocomplete/autofill is disabled (\"off\") by default. Specify \"on\" to enable browser autocomplete/autofill or for further control, specify an autofill field name. See <a href=\"https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill-field\" target=\"_blank\">here</a> for details on autofill field names.") },
+    { name: "browser auto complete", hideFormatting: true, choices: ["on", "off", "Other..."], controls: ["combo box", "date field", "password field", "textbox", "spinner"], help: pui.helpTextProperties("off", "Specifies the value of the HTML textbox \"autocomplete\" attribute, which controls the browser's autocomplete/autofill feature. Browser autocomplete/autofill is disabled (\"off\") by default. Specify \"on\" to enable browser autocomplete/autofill or for further control, specify an autofill field name. See <a href=\"https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#autofill-field\" target=\"_blank\">here</a> for details on autofill field names.") },
     { name: "related field", help: pui.helpTextProperties("blank", "This property allows you to create a radio button group by associating multiple radio buttons with a field from the original application. Specify the id of the field to associate the radio button with. Additionally, this property can associate a text area with a group of textboxes by specify a comma separated list of textbox id's."), controls: ["radio button", "text area"], context: "genie" },
     { name: "select box height", format: "number", attribute: "size", choices: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "Other..."], help: pui.helpTextProperties("5", "If specified, the select box appears as a list box; if omitted, the select box appears as a dropdown.", ["other"]), controls: ["select box"] },
     { name: "multiple", attribute: "multiple", choices: ["true", "false"], type: "boolean", help: pui.helpTextProperties("false", "Specifies that multiple options can be selected at once in a List Box. The options are returned as a comma separated list."), controls: ["select box"], context: "dspf", hideFormatting: true, validDataTypes: ["indicator", "expression"] },

proddata/js/widgets/combo_box.js
@@ -72,7 +72,8 @@ pui.ComboBoxWidget = function() {
     }
 
     box.type = "text";
-    if (box.getAttribute("autocomplete") == null) // Default off if not already set by 'html auto complete' property.
+    // Default off if not set by 'html auto complete' property.
+    if (box.getAttribute("autocomplete") == null && (context != "genie" || !pui.genie.config.browserAutoComplete))
       box.setAttribute("autocomplete", "off");
     box.style.position = "absolute";
     box.style.border = "0px none";
@@ -669,7 +670,7 @@ pui.widgets.add({
       catch(e) { }
     },
     
-    "html autocomplete": function(parms) {
+    "browser auto complete": function(parms) {
       if (!parms.design && parms.dom.comboBoxWidget != null)
         parms.dom.comboBoxWidget.getBox().setAttribute("autocomplete", parms.value);
     }

proddata/js/widgets/date_field.js
@@ -598,7 +598,8 @@ pui.widgets.add({
             }
             
           }, 1);
-          if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+          // Default off if not set by 'html auto complete' property.
+          if (parms.dom.getAttribute("autocomplete") == null && (context != "genie" || !pui.genie.config.browserAutoComplete))
             parms.dom.setAttribute("autocomplete", "off");
         }
         if (pui.iPadEmulation && !pui.iPhoneEmulation) {
@@ -671,7 +672,7 @@ pui.widgets.add({
         }
       }
     },
-    "html autocomplete": function(parms) {
+    "browser auto complete": function(parms) {
       if (!parms.design)
         parms.dom.setAttribute("autocomplete", parms.value);
     }

proddata/js/widgets/password_field.js
@@ -38,7 +38,8 @@ pui.widgets.add({
             getObj("ipadKeyboard").style.display = "none";
           });
         }
-        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+        // Default off if not set by 'html auto complete' property.
+        if (parms.dom.getAttribute("autocomplete") == null && (context != "genie" || !pui.genie.config.browserAutoComplete))
           parms.dom.setAttribute("autocomplete", "off");
       }
     },
@@ -47,7 +48,7 @@ pui.widgets.add({
       parms.dom.value = parms.value;
     },
     
-    "html autocomplete": function(parms) {
+    "browser auto complete": function(parms) {
       if (!parms.design)
         parms.dom.setAttribute("autocomplete", parms.value);
     }

proddata/js/widgets/spinner.js
@@ -236,7 +236,8 @@ pui.widgets.add({
             parms.dom.spinner.positionSpinnButtons();
           }
         }, 1);
-        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+        // Default off if not set by 'html auto complete' property.
+        if (parms.dom.getAttribute("autocomplete") == null && (context != "genie" || !pui.genie.config.browserAutoComplete))
           parms.dom.setAttribute("autocomplete", "off");
       }
       else {
@@ -291,7 +292,7 @@ pui.widgets.add({
       }
     },
     
-    "html autocomplete": function(parms) {
+    "browser auto complete": function(parms) {
       if (!parms.design)
         parms.dom.setAttribute("autocomplete", parms.value);
     }

proddata/js/widgets/textbox.js
@@ -46,7 +46,8 @@ pui.widgets.add({
           parms.dom.spellcheck = pui["allow spellcheck"];
         //If they are changing a date field to a textbox, remove the calendar
         if(parms.dom.calimg) pui.removeCal(parms.dom);
-        if (parms.dom.getAttribute("autocomplete") == null) // Default off if not set by 'html auto complete' property.
+        // Default off if not set by 'html auto complete' property.
+        if (parms.dom.getAttribute("autocomplete") == null && (context != "genie" || !pui.genie.config.browserAutoComplete))
           parms.dom.setAttribute("autocomplete", "off");
       }
       if (parms.design) { 
@@ -76,7 +77,7 @@ pui.widgets.add({
       }
     },
     
-    "html autocomplete": function(parms) {
+    "browser auto complete": function(parms) {
       if (!parms.design)
         parms.dom.setAttribute("autocomplete", parms.value);
     }