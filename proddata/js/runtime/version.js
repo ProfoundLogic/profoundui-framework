4bb7567181f43295afb63fd3043fe1212c804494
Kyle Law <klaw@profoundlogic.com>
2019-05-30 18:30:56 -0400

    Update Profound UI version to 6.3.2

--- proddata/js/runtime/version.js
+++ proddata/js/runtime/version.js
@@ -22,5 +22,5 @@
 
 if (!window["pui"]) window["pui"] = {};
 window["pui"]["baseVersion"] = '6';
-window["pui"]["fixPackVersion"] = '3.1';
+window["pui"]["fixPackVersion"] = '3.2';
 window["pui"]["version"] = window["pui"]["baseVersion"] + "." + window["pui"]["fixPackVersion"];
