f88212053777b439f7b15778b690d09fa083bcf5
Scot Roach <sroach@profoundlogic.com>
2020-12-09 17:45:02 -0600

    Added new utility function - pui.getUniqueString

--- proddata/js/runtime/utils.js
+++ proddata/js/runtime/utils.js
@@ -5026,3 +5026,18 @@ pui.getVersionComparer = function () {
     }
   }
 }
+
+pui["generateUniqueString"] = function () {
+  var d = new Date().getTime();
+
+  if (window["performance"] && typeof window["performance"]["now"] === "function")
+      d += ["performance"]["now"]();
+
+  var value = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/x/g, function (c) {
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+  });
+
+  return value;
+}
