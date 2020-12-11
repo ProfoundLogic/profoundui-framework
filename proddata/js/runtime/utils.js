bbcb94b00d428c7e49f26abc2fbf7f46202720a4
Scot Roach <sroach@profoundlogic.com>
2020-12-11 09:34:28 -0600

    Renamed new pui API to pui.newUUID()

--- proddata/js/runtime/utils.js
+++ proddata/js/runtime/utils.js
@@ -5027,13 +5027,13 @@ pui.getVersionComparer = function () {
   }
 }
 
-pui["generateUniqueString"] = function () {
+pui["newUUID"] = function () {
   var d = new Date().getTime();
 
   if (window["performance"] && typeof window["performance"]["now"] === "function")
-      d += ["performance"]["now"]();
+      d += window["performance"]["now"]();
 
-  var value = 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'.replace(/x/g, function (c) {
+  var value = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
       var r = (d + Math.random() * 16) % 16 | 0;
       d = Math.floor(d / 16);
       return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
