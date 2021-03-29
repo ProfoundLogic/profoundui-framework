ProfoundLogic/profoundui-framework

commit 7a7727f7d043db7fdcaaec136df179e716193b53

6640. Fixed Signature pad not working inside mobile scroller layout. Also, removed Layout code that catches clicks globally and manually fires tap events in iOS--no longer needed.

proddata/js/widgets/layout/Layout.js
@@ -579,96 +579,61 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
   }
 };
 
+/**
+ * If the layout's ID or className is "scroller", then applyScrolling is called to setup IScroll during the property setter initialize.
+ */
 pui.layout.Layout.prototype.applyScrolling = function() {
-  // Note: the closured, "me", saves us from reworking the functions below while allowing applyScrolling to be in the prototype.
   var me = this;
-  function setupiScroll() {
+  /**
+   * setupiScroll can be called after iscroll.js is loaded and IScroll is a function.
+   * Tests: make sure CSS buttons can be clicked when inside scrollers #4590. Make sure signature pads can be signed #6640.
+   */
+  function setupiScroll(){
+    clearTimeout(setupiScroll_tmo);  //If the loadJS callback is called before the setTimout callback, then avoid duplicate calls.
     var parent = me.layoutDiv.parentNode;
     if (parent != null && parent.tagName == "DIV") {
-      if (pui["is_ios"]) {
-
-        document.body.addEventListener('tap', function (e) {
-          e["preventDefault"]();
-          e["stopPropagation"]();
-          e["stopImmediatePropagation"]();
-          var event = new MouseEvent('click',{
-            view: window,
-            bubbles: true,
-            cancelable: true
-          });
-          var target = getTarget(e);
-          if (target) {
-            if (!/^(INPUT|TEXTAREA|BUTTON|SELECT|IMG)$/.test(target.tagName)) {
-
-              // Avoid firing onclick more than once per tap, as happens in 'scroller' classes. If last handled click was > 2 seconds ago, just fire. #5169.
-              if (pui.iscrolltapped == null || (new Date() - pui.iscrolltapped) > 2000 ){
-                setTimeout(function() {
-                  var isCanceled = target.dispatchEvent(event);
-                  if (!isCanceled) {
-                    event["preventDefault"]();
-                    event["stopPropagation"]();
-                  }
-                },10);
-              }
-              pui.iscrolltapped = new Date();  //Prevent next click; this will be cleared in 'onclick' in properties.js.
-            }
-          } 
-        }, false);
-      }
       var config = {
         "scrollbars": true,
         "mouseWheel": true,
-        "shrink": true,
-        "tap": pui["is_ios"],
         "disableMouse": pui["is_ios"],
         "preventDefaultException": {
           tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT|IMG)$/
-        },
-        "onBeforeScrollStart": function (e) {
-          var target = getTarget(e);
-          while (target.nodeType != 1) target = target.parentNode;
-          while (target.tagName == "SPAN") target = target.parentNode;
-          if (target.tagName != "SELECT" && target.tagName != "INPUT" && target.tagName != "TEXTAREA" && target.tagName != "A") {
-            e.preventDefault();
-          }
         }
       };
-      if (typeof IScroll == "function") me.iScroll = new IScroll(parent, config);  // new version
-      else me.iScroll = new iScroll(parent, config);  // old version
+      this.iScroll = new IScroll(parent, config);
     }
   }
 
   var counter = 0;
-
+  
   function keepTryingToSetupiScroll() {
     counter++;
     if (counter > 100) {  // give up
       return;
     }
-    setTimeout(function() {
+    setupiScroll_tmo = setTimeout(function() {
       if (typeof IScroll == "function" || typeof iScroll == "function") {  // as of version 5, the class name is IScroll (used to be iScroll)
         setupiScroll();
       }
       else {
         keepTryingToSetupiScroll();
-      }        
+      }
     }, 200);
   }
-
+  
+  var setupiScroll_tmo = 0;
   if (typeof IScroll == "function" || typeof iScroll == "function") {  // as of version 5, the class name is IScroll (used to be iScroll)
     setupiScroll();
   }
   else {
     var returnValue = pui["loadJS"]({
       "path": pui.normalizeURL("/iscroll/iscroll.js"),
-      "callback": function() {
-        setupiScroll();
-      }
+      "callback": setupiScroll
     });
     if (returnValue == false) {
       keepTryingToSetupiScroll();
     }
-  }      
+  }
 };
 
 /**

proddata/js/runtime/api.js
@@ -1888,6 +1888,11 @@ pui["removeCSS"] = function(path) {
   }
 };
 
+/**
+ * Load a JavaScript source file.
+ * @param {Object|String} parms    A string URL or an object with parameters.
+ * @returns {Boolean}  Returns false if the argument was incorrect or if the script was already loaded; otherwise returns true.
+ */
 pui["loadJS"] = function(parms) {
   if (parms == null) return null;
   var path;

proddata/js/runtime/properties.js
@@ -1491,10 +1491,6 @@ function applyPropertyToField(propConfig, properties, domObj, newValue, isDesign
         pui["temp_event"] = e;
         eval("var event = pui.temp_event;");
         try {
-          if (pui["is_ios"]) {  
-            delete pui.iscrolltapped;  //Allow next CSS button tap to fire onclick. #5169.
-          }
-          
           var customFunction = eval(newValue);
           if (typeof customFunction == "function") {
             if (!e) e = window.event;

proddata/js/widgets/signature_pad.js
@@ -215,7 +215,11 @@ pui.SignaturePad = function() {
       moveBox.style.cursor = "move";
       me.container.appendChild(moveBox);
     }
-    me.resize();    
+    me.resize();
+    // In case the canvas is in an IScroll, prevent the IScroll from handling the event.
+    // pointerdown is captured before mousedown, making IScroll scroll. #6640.
+    canvas.addEventListener('pointerdown', function(e){ e.stopPropagation(); });
+    
     addEvent(canvas, "mousedown", startDrawing);
     addEvent(canvas, "touchstart", startDrawing);
     addEvent(me.container, "dblclick", function(e) {

commit 6d9f7fe64d665c1c844af4eb88312f304a81a1f0

Fixed bug with mobile scroller where scrolling stopped working on mobile clients broken by 7a7727f7

proddata/js/widgets/layout/Layout.js
@@ -600,7 +600,7 @@ pui.layout.Layout.prototype.applyScrolling = function() {
           tagName: /^(INPUT|TEXTAREA|BUTTON|SELECT|IMG)$/
         }
       };
-      this.iScroll = new IScroll(parent, config);
+      me.iScroll = new IScroll(parent, config);
     }
   }
 