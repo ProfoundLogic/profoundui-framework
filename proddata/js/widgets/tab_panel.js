ProfoundLogic/profoundui-framework

commit 72e749f3bb1a2e0d4f4558c0d8eb76e864da25a8

6468. Changes to TabLayout to fix its template re-applying for every property.

proddata/js/widgets/tab_panel.js
@@ -193,13 +193,13 @@ function TabPanel() {
   var scrollCounter = 0;
   
   // Clicking a tab redraws the panel, so preserve the scrollLeft with this.
-  var lastScrollLeft = null;
+  this._lastScrollLeft = null;
   
   // This value is used in 3 places for themes that don't use images.
   var simpleSelectedTabBackgroundColor = "#B7C8F6";
   
   // This is used to hide tabs dynamically
-  var hiddenTabs = {};
+  this._hiddenTabs = {};
   
   // Tab style information when tab panel styles are hard-coded.
   var settings;      //The settings for the set tab style.
@@ -222,8 +222,6 @@ function TabPanel() {
   
   this.tabStyle = "Simple";
   
-  this.INITIALLIST = "Tab 1,Tab 2,Tab 3";
-  
   //Holds all the tabs. Needs to be in class for add/removeIconOnClick.
   var topDiv = null;
   
@@ -239,7 +237,7 @@ function TabPanel() {
       me.selectedTab = 0;
       if (me.selectedTab == index) me.selectedTab = 1;
     }
-    hiddenTabs[index] = true;
+    this._hiddenTabs[index] = true;
     me.selectedTabChanged();
   };
   
@@ -249,17 +247,8 @@ function TabPanel() {
    * @returns {undefined}
    */
   this.showTab = function(index) {
-    hiddenTabs[index] = false;
+    this._hiddenTabs[index] = false;
     me.selectedTabChanged();
-  };
-  
-  /**
-   * Returns true if the tab at the index is set to be hidden; else false. Needed by subclass.
-   * @param {Number} index
-   * @returns {Boolean}
-   */
-  this.isHidden = function(index){
-    return hiddenTabs[index];
   };
   
   // API Methods attached as container element's properties of the same name.
@@ -414,7 +403,7 @@ function TabPanel() {
       // Create a parent span to encapsulate the tab text, left, and right border.
       var outerSpan = document.createElement("span");
       outerSpan.style.display = "inline-block";
-      if (hiddenTabs[i]) outerSpan.style.display = "none";
+      if (this._hiddenTabs[i]) outerSpan.style.display = "none";
       
       if (i == me.selectedTab) {
         if (me.container.className != "")
@@ -603,13 +592,13 @@ function TabPanel() {
   this.processTabChange = function(tab){
     var dom = me.container;
     if (dom.disabled != true) {
-      if (context == "dspf" && dom.sendTabResponse == true) {
+      if (context == "dspf" && me.sendTabResponse == true) {
         dom.responseValue = tab;
         if (dom.bypassValidation == "true" || dom.bypassValidation == "send data") {
           pui.bypassValidation = dom.bypassValidation;
         }
-        if (dom.responseAID) {
-          pui.keyName = dom.responseAID;
+        if (me.responseAID) {
+          pui.keyName = me.responseAID;
         }
         var returnVal = pui.respond();
         if (returnVal == false){  
@@ -619,7 +608,7 @@ function TabPanel() {
       }
       else {
         me.selectedTabChanged();
-        if (context == "dspf" && dom.sendActiveTab == true) {
+        if (context == "dspf" && me.sendActiveTab == true) {
           dom.responseValue = tab;
         }
       }
@@ -677,13 +666,11 @@ function TabPanel() {
    */
   this.tabSpanOnclick = function(e) {
     var target = getTarget(e);
-    lastScrollLeft = target.parentNode.parentNode.scrollLeft; //topDiv is 2 nodes up.
+    me._lastScrollLeft = target.parentNode.parentNode.scrollLeft; //topDiv is 2 nodes up.
     if (target.tabId == null && target.parentNode.tabId != null) target = target.parentNode;
     var tab = target.tabId;
-    if (me.container.ontabclick != null) {
-      var returnVal = me.container.ontabclick(tab);
-      if (returnVal == false) return;
-    }
+    if (typeof me.tabSpanOnclickCb === 'function' && me.tabSpanOnclickCb(tab) == false) return;  //the callback is implemented in TabLayout.
+
     if (me.container.tabKeys != null) {
       var tabKeysArray = me.container.tabKeys.split(",");
       if (tabKeysArray.length - 1 >= tab) {
@@ -790,7 +777,7 @@ function TabPanel() {
     
     // Setup the tab area to scroll all the way to the right; happens when scroll buttons are handled.
     if (topDiv){
-      lastScrollLeft = topDiv.scrollWidth;  //This is too large but will be fixed later.
+      me._lastScrollLeft = topDiv.scrollWidth;  //This is too large but will be fixed later.
     }
     
     var nmodel = getPropertiesNamedModel();
@@ -860,7 +847,7 @@ function TabPanel() {
         itm.designer.propWindow.refreshProperty("tab names");
         
         // If a new TabLayout wasn't created, then applyProperty failed, and selectedTab should be restored.
-        if (itm.dom.tabLayout != null && itm.dom.tabLayout == me){
+        if (itm.dom.layoutT != null && itm.dom.layoutT == me){
           me.selectedTab = origSelectedTab;
         }
       }
@@ -875,17 +862,17 @@ function TabPanel() {
    * @returns {undefined}
    */
   function tabLayoutPreserveValues(itm){
-    if (itm.dom.tabLayout != null && itm.dom.tabLayout != me ){
-      itm.dom.tabLayout.selectedTab = me.selectedTab;   //Make sure the new tab is selected.
-      itm.dom.tabLayout.setLastScrollLeft(lastScrollLeft);  //Scroll to the new tab.
-      itm.dom.tabLayout.selectedTabChanged();
-      itm.dom.tabLayout.checkScrollButtons(); //scrolls to active and shows buttons, if necessary.
+    if (itm.dom.layoutT != null && itm.dom.layoutT != me ){
+      itm.dom.layoutT.selectedTab = me.selectedTab;   //Make sure the new tab is selected.
+      itm.dom.layoutT.setLastScrollLeft(me._lastScrollLeft);  //Scroll to the new tab.
+      itm.dom.layoutT.selectedTabChanged();
+      itm.dom.layoutT.checkScrollButtons(); //scrolls to active and shows buttons, if necessary.
     }
   }
   
   // Allow tabLayoutPreserveValues to set a private variable (inside a closure).
   this.setLastScrollLeft = function(lsl){
-    lastScrollLeft = lsl;
+    me._lastScrollLeft = lsl;
   };
   
   /**
@@ -941,15 +928,15 @@ function TabPanel() {
     
     // Scroll to the selected tab if "active tab" is bound. When "active tab" is bound, then
     // the tabpanel is re-constructed each ExFmt; so lastScrollLeft would be null at first.
-    if( lastScrollLeft === null )
+    if( me._lastScrollLeft === null )
     {
       // The selected tab number should correspond to the array index of childNodes.
       if( topDiv.childNodes != null && me.selectedTab >= 0 && me.selectedTab < topDiv.childNodes.length){
         var outerSpan = topDiv.childNodes[me.selectedTab];
         // Put the selected tab in the middle of the tab panel.
-        lastScrollLeft = Math.round(outerSpan.offsetLeft - topDiv.offsetWidth / 2  + outerSpan.offsetWidth / 2);
+        me._lastScrollLeft = Math.round(outerSpan.offsetLeft - topDiv.offsetWidth / 2  + outerSpan.offsetWidth / 2);
       }
-      else lastScrollLeft = 0; //Or set to 0 for later math. (This case shouldn't normally happen.)
+      else me._lastScrollLeft = 0; //Or set to 0 for later math. (This case shouldn't normally happen.)
     }
     
     me.checkScrollButtons();
@@ -1024,14 +1011,14 @@ function TabPanel() {
 
     // Avoid showing scroll buttons if we are close enough to an end. Fixes
     // button appearing/disappearing when Angle tabs shift by 1 pixel on click.
-    if( (topDiv_scrollLeftMax - lastScrollLeft) <= 3) lastScrollLeft = topDiv_scrollLeftMax;
-    else if( lastScrollLeft <= 3 ) lastScrollLeft = 0;
+    if( (topDiv_scrollLeftMax - me._lastScrollLeft) <= 3) me._lastScrollLeft = topDiv_scrollLeftMax;
+    else if( me._lastScrollLeft <= 3 ) me._lastScrollLeft = 0;
 
     // Restore the previous scrollLeft from before a tab was clicked, or to
     // scroll to the active tab.
     // Note: Emulated IE8 sometimes gets Unspecified error setting topDiv.scrollLeft, and I can find no fix.
     // However, actual IE8 in Win XP has no problem with this. So ignore error. MD.
-    try{topDiv.scrollLeft = lastScrollLeft;}catch(exc){}
+    try{topDiv.scrollLeft = me._lastScrollLeft;}catch(exc){}
 
     // Display either button only when needed.
     if( topDiv.scrollLeft > 0 ){
@@ -1126,6 +1113,8 @@ function TabPanel() {
 }
 TabPanel.prototype = Object.create(pui.BaseClass.prototype);  //inherit deleteOwnProperties.
 
+// Static constant all TabPanel and TabLayout instances can use.
+Object.defineProperties(TabPanel.prototype, { INITIALLIST: { value: "Tab 1,Tab 2,Tab 3" } });
 
 pui.widgets.add({
   name: "tab panel",
@@ -1224,18 +1213,11 @@ pui.widgets.add({
     },
     
     "response AID": function(parms) {
-    
-      if (parms.value && parms.value != "") {
-      
-        parms.dom.responseAID = parms.value;
-      
+      if (parms.dom.tabPanel){
+        var value = parms.value;
+        if (typeof value !== 'string' || value.length === 0) value = null;
+        parms.dom.tabPanel.responseAID = parms.value;
       }
-      else {
-      
-        parms.dom.responseAID = null;
-      
-      }
-    
     }
  
   }

proddata/js/runtime/dspf/render.js
@@ -1772,10 +1772,12 @@ pui.renderFormat = function(parms) {
                   dom.responseValue = "";
                 }
                 if (propname == "tab response") {
-                  dom.sendTabResponse = true;
+                  var cls = dom.tabPanel; // || dom.layoutT;
+                  if (cls) cls.sendTabResponse = true;
                 }
                 if (propname == "active tab") {
-                  dom.sendActiveTab = true;
+                  var cls = dom.tabPanel; // || dom.layoutT;
+                  if (cls) cls.sendActiveTab = true;
                 }
                 if (propname == "response") {
                   var shortcutKey = properties["shortcut key"];
@@ -2839,9 +2841,9 @@ pui.renderFormat = function(parms) {
     var tabElem = document.getElementById(tabPanels[i]);
     var tabPanel = tabElem.tabPanel;
     if (!tabPanel){
-      tabPanel = tabElem.tabLayout;
+      tabPanel = tabElem.layoutT;
     }
-    if (tabPanel){
+    if (tabPanel && typeof tabPanel.selectedTabChanged === 'function'){
       if (activeTabs[i]) tabPanel.selectedTab = activeTabs[i];
       tabPanel.selectedTabChanged();  //Re draws the tabs.
     }

proddata/js/runtime/utils.js
@@ -1455,9 +1455,10 @@ pui.BaseClass = function(){};
 
 /**
  * Utility for deleting class members. This should be called from a child's prototype.destroy method by: "this.deleteOwnProperties();".
+ * It may also be called like this: pui.BaseClass.prototype.deleteOwnProperties.call(someObject);
  */
 pui.BaseClass.prototype.deleteOwnProperties = function(){
-  if (this != window){
+  if (this != null && this != window && this != document){
     var propnames = Object.getOwnPropertyNames(this);
     for (var i=0, n=propnames.length; i < n; i++){
       try {

proddata/js/widgets/layout/Layout.js
@@ -74,7 +74,7 @@ pui.layout.Layout.prototype._sizeContainer = function(container) {
 
 /**
  * For each child in the specified container, notify that it is visible. This is necessary if certain code
- * in the child layout doesn't work until the layout is on the DOM and visible; e.g. tabLayout scroll buttons and date fields, charts in Chrome.
+ * in the child layout doesn't work until the layout is on the DOM and visible; e.g. TabLayout scroll buttons and date fields, charts in Chrome.
  * @param {Object} container
  */
 pui.layout.Layout.prototype._notifyChildrenVisible =  function(container){
@@ -345,15 +345,9 @@ pui.layout.Layout.prototype.resize = function() {
   var accordion = this.layoutDiv.accordion;
   var layoutTClass = this.layoutDiv.layoutT;
   
-//  var tabLayout = this.layoutDiv.tabLayout;
-  
-  
-  
-  
   if (panel) panel.resize();
   else if (accordion) accordion.resize();
   else if (layoutTClass) layoutTClass.resize();
-//  else if (tabLayout) tabLayout.resize();
 };
 
 /**
@@ -367,12 +361,9 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
   var accordion = this.layoutDiv.accordion;
   
   // If the layout template's class handles setting properties, then return. Those should prevent custom properties from re-loading 
-  // the entire template when not defined here.
-  var tabLayout = this.layoutDiv.tabLayout;
-  if (tabLayout && tabLayout.setProperty(property, value)) return;
-  
+  // the entire template when not defined here.  
   var layoutT = this.layoutDiv.layoutT;
-  if (layoutT && layoutT.setProperty(property, value)) return;
+  if (layoutT && layoutT.setProperty(property, value, this.templateProps)) return;
 
   switch (property) {
     case "id":
@@ -383,7 +374,7 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
       break;
 
     case "template":
-      if (this.designMode) {
+      if (this.designMode) {        
         this.designItem.properties["template"] = value;
         this.designItem.propertiesChanged["template"] = true;
         this.designItem.changed = true;
@@ -416,8 +407,7 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
       
       if (panel != null) panel.resize();
       else if (accordion != null) accordion.resize();
-      else if (layoutT != null) layoutT.resize();   //responsive layout, fieldset, etc.
-      else if (tabLayout != null) tabLayout.resize();
+      else if (layoutT != null) layoutT.resize();   //responsive layout, fieldset, Tab Layout, etc.
       
       this.stretch();
 
@@ -558,14 +548,11 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
     case "text decoration":
     case "text transform":
       if (panel != null) panel.setStyle(property, value);
-      if (accordion != null) {
+      else if (accordion != null) {
         accordion.setStyle(property, value);
         if (property == "font family" || property == "font size") {
           accordion.resize();
         }
-      }
-      if (tabLayout != null) {
-        tabLayout.setStyle(property, value);
       }
       this.templateProps[property] = value;
       break;
@@ -582,6 +569,10 @@ pui.layout.Layout.prototype.setProperty = function(property, value) {
           }
         };
       }
+      break;
+      
+    case 'lazy load':
+      // Do nothing here; renderFormat handles this property. Catch this case to prevent applyTemplate from rebuilding unnecessarily.
       break;
 
     case "onlazyload":
@@ -851,14 +842,10 @@ pui.layout.Layout.prototype['handleEvent'] = function(e) {
  * @constructor
  */
 pui.layout.Template = function(parms, dom) {
-  this.container = dom;
   this.forProxy = (parms && parms.proxyMode);
   this.designMode = (parms && parms.designMode);
   
-  // Lets pui.Layout and applyTemplate know that the dom contains this class (or it is a TabLayout that implements the same functions.)
-  dom.layoutT = this;
-  
-  dom.sizeMe = this.resize.bind(this);
+  if (dom) this.linkToDom(dom);
 };
 pui.layout.Template.prototype = Object.create(pui.BaseClass.prototype);
 
@@ -872,25 +859,41 @@ pui.layout.Template.prototype.resize = function() {
 };
 
 /**
- * Placeholder for subclasses to implement and override.
+ * Placeholder for subclasses to implement and override. Each template needs to handle its template-specific properties; 
+ * otherwise, the Layout's setProperty calls applyTemplate, rebuilding the layout.
+ * Handle properties so that applyTemplate isn't called for every property being set, rebuilding the layout.
  * @param {String} property
  * @param {String} value
+ * @param {Object} templateProps  A collection in pui.Layout with properties for this template.
  * @returns {Boolean}  When true is returned, the pui.Layout.prototype.setProperty will not process the property change any more.
  */
-pui.layout.Template.prototype.setProperty = function(property, value){
+pui.layout.Template.prototype.setProperty = function(property, value, templateProps){
   return false;
 };
 
 /**
- * Assign template-specific properties to the DOM element that is passed to applyTemplate. TODO: this approach seems overcomplicated:
+ * Assign template-specific properties to a DOM element. called by applyTemplate in the pui.layout.Template constructor.
+ * 
+ * TODO: this approach seems overcomplicated:
  * templates get built on an object detached from the DOM and then properties from that DOM are copied onto an existing DOM element.
  * Why not just evaluate the template in the "field type" or "template" setters before constructing the appropriate Layout subclass.
  * Then you would know if the template is good, and there's no need to construct a fake one that you can back out of in case something 
  * goes wrong.
- * @param {Element} dom
+ * @param {Element} dom    Should not be null or undefined.
  */
-pui.layout.Template.prototype.assignToOtherDom = function(dom){
-  dom.layoutT = this;
-  dom.sizeMe = this.resize.bind(this);
+pui.layout.Template.prototype.linkToDom = function(dom){
   this.container = dom;
+  // "layoutT" lets pui.Layout and applyTemplate know that the dom contains this class (or it is a TabLayout that implements the same functions.)
+  this.container.layoutT = this;
+  this.container.sizeMe = this.resize.bind(this);   //rendering framework calls sizeMe sometimes.
+};
+
+/**
+ * Clean up any properties that this class added to the DOM, and then clear this.
+ */
+pui.layout.Template.prototype.destroy = function(){
+  var deleteOwn = pui.BaseClass.prototype.deleteOwnProperties;
+  // delete all properties added to this.container: sizeMe, layoutT, etc.
+  if (this.container) deleteOwn.call(this.container);
+  deleteOwn(this); //delete any properties added to "this" object.
 };

proddata/js/widgets/layout/ResponsiveLayout.js
@@ -63,29 +63,6 @@ pui.ResponsiveLayout = function(parms, dom){
       // The node will disappear automatically when a new screen is rendered, because the 5250 div is cleared.
     }
   }
-    
-  dom.sizeMe = this.resize.bind(this);
-  
-  // Map special setters to properties. These are called at the end of setProperty in pui.Layout, which is
-  // the global property setter for "layout" widgets. When one property changes, each are evaluated in this order.
-  if (parms && parms.properties){
-    var properties = parms.properties;
-    if (properties["container names"] != null) {
-      this.setContainerNames(properties["container names"]);  //setNumItems depends on the value set here.
-    }
-
-    if (properties["layout items"] != null) {
-      this.setNumItems(properties["layout items"]);
-    }
-
-    if (properties["use viewport"] != null) {
-      this.setUseViewport(properties["use viewport"] != "false");
-    }
-
-    if (properties["style rules"] != null) {
-      this.setRules(properties["style rules"]);
-    }
-  }
 };
 pui.ResponsiveLayout.prototype = Object.create(pui.layout.Template.prototype);
 
@@ -114,12 +91,12 @@ pui.ResponsiveLayout.prototype.sentToBackground = function(){
 };
 
 pui.ResponsiveLayout.prototype.destroy = function(){
-  this.deleteOwnProperties(); //from pui.BaseClass.
+  if (this.container) this.container.removeAttribute("puirespbg");
+  pui.layout.Template.prototype.destroy.call(this); //remove properties attached to this.container, call deleteOwnProperties.
 };
 
 /**
  * Handle when the number of items is changed or set.
- * (This is called because it is set in the constructor.)
  * Pre-Condition: This.container must be set to some node before this is called.
  * @param {Number} numitems
  * @returns {undefined}
@@ -152,7 +129,9 @@ pui.ResponsiveLayout.prototype.setNumItems = function(numitems) {
       this._setContainerName(i, div);
 
       this._mainnode.appendChild(div);
-    } 
+    }
+    
+    if (this._stylenode) this._mainnode.appendChild(this._stylenode); //The style node was also removed, so restore it.
   }
 
   if (this.designMode && !this.forProxy && typeof this.addDesignerIcons == "function" ){
@@ -161,11 +140,14 @@ pui.ResponsiveLayout.prototype.setNumItems = function(numitems) {
   }
 };
 
-// Note: setRules runs after this, because setRules is last in the constructor.
-// So, changing the "use viewport" option causes rules to be re-evaluated.
-pui.ResponsiveLayout.prototype.setUseViewport = function(usev) {
+/**
+ * Change the useViewport flag, and re-evaluate the style rules.
+ * @param {Boolean} usev
+ */
+pui.ResponsiveLayout.prototype._setUseViewport = function(usev) {
   if (usev == null || usev === "" || pui.isBound(usev)) return;
   this._useViewport = usev;
+  this.setRules();
 };
 
 /**
@@ -359,7 +341,7 @@ pui.ResponsiveLayout.prototype._manipulateCSSOM = function() {
   this._findRulesContToCheck = this._getContainerToCheck();
   this._findRulesCssText = "";
   this._findRulesRegex2.lastIndex = 0;  //ensure the search starts at 0 in case a previous "g" flag updated .lastIndex.
-  this._findRules(this._stylenode.sheet);
+  if (this._stylenode.sheet != null) this._findRules(this._stylenode.sheet); //Note: the style node may be detached from the DOM while resize is called.
   this._stylenode.textContent = this._findRulesCssText;
   delete this._findRulesContToCheck;
 };
@@ -501,6 +483,7 @@ pui.ResponsiveLayout.prototype.parseSectionSizes = function(str) {
 }; //end parseSectionSizes().
 
 /**
+ * Handle properties so that applyTemplate isn't called for every property being set, rebuilding the layout.
  * @param {String} property
  * @param {String} value
  * @returns {Boolean}  When true is returned, the caller, pui.Layout's setProperty, will return after this function returns.
@@ -513,9 +496,25 @@ pui.ResponsiveLayout.prototype.setProperty = function(property, value){
       // The responsive layout's embedded styles can use the widget's ID. So these must be refreshed.
       this.setRules();
       break;
+            
+    case 'layout items':
+      this.setNumItems(value);
+      break;
+      
+    case 'style rules':
+      this.setRules(value);
+      break;
+      
+    case 'use viewport':
+      this._setUseViewport(value != "false");
+      break;
+      
+    case 'container names':
+      this.setContainerNames(value);  //setNumItems depends on the value set here.
+      break;
       
     default:
-      ret = false;
+      ret = false;  //Let the Layout's setProperty handle anything else.
       break;
   }
   return ret;

proddata/js/widgets/layout/applyTemplate.js
@@ -96,41 +96,7 @@ pui.layout.template.applyTemplate = function(parms) {
   }
   
   if (newDom.layoutT != null){
-    newDom.layoutT.assignToOtherDom(dom);   //This handles everything necessary for whatever type of class is in layoutT.
-  }
-  
-  if (newDom.tabLayout != null){
-    var selectedTab;
-    if (dom.tabLayout != null){
-      selectedTab = dom.tabLayout.selectedTab;
-    }
-    dom.tabLayout = newDom.tabLayout;
-    dom.sizeMe = dom.tabLayout.resize;
-    dom.tabLayout.container = dom;
-    dom.layout.getActiveContainerNumbers = newDom.tabLayout.getActiveContainerNumbers; //Needed by lazy-load.
-    dom.layout.notifyvisibleOnce = newDom.tabLayout.resize;  //Needed for scroll buttons when child is in hidden tab/section. Also for #4711.
-    
-    // Preserve the active tab when a template property is changed; e.g. tab names.
-    if (selectedTab != null && !isNaN(selectedTab) && selectedTab >= 0  
-    && selectedTab < dom.tabLayout.tabs.length && selectedTab != dom.tabLayout.selectedTab){
-      dom.tabLayout.selectedTab = selectedTab;
-      dom.tabLayout.selectedTabChanged();
-    }
-    
-    dom.tabLayout.containerInDom(); //Finish drawing things that require elements being in DOM.
-    
-    dom.sendTabResponse = newDom.sendTabResponse;
-    dom.sendActiveTab = newDom.sendActiveTab;
-    dom.responseAID = newDom.responseAID;
-    
-    if (!parms.designMode){
-      // Setup APIs.
-      dom.setTab = dom.tabLayout.setTab;
-      dom.getTab = dom.tabLayout.getTab;
-      dom.refresh = dom.tabLayout.refresh;
-      dom["hideTab"] = dom.tabLayout.hideTab;
-      dom["showTab"] = dom.tabLayout.showTab;
-    }
+    newDom.layoutT.linkToDom(dom);  //This handles everything necessary for whatever type of class is in layoutT: TabLayout, Responsive, etc.
   }
   
   return { 

proddata/js/widgets/layout/layoutWidget.js
@@ -107,6 +107,9 @@ pui.widgets.add({
       if (parms.propertyName == "template") {
         savedTemplateName = parms.dom.layout.template;
         parms.dom.layout.template = parms.value;
+        
+        // The template switched, so the old one needs to be destroyed to avoid things like resize() being called on a stale reference.
+        if (parms.value != parms.properties["template"] && parms.dom.layoutT) parms.dom.layoutT.destroy();
       }
       else if (!parms.design) {
         window.addEventListener('resize', parms.dom.layout);

commit be8edd76cdd72925574186079bb5bad295d96038

6772. Fixed ontabclick for tab panel broken by 72e749f3.

proddata/js/widgets/tab_panel.js
@@ -672,7 +672,12 @@ function TabPanel() {
     if (target.tabId == null && target.parentNode.tabId != null) target = target.parentNode;
     var tab = target.tabId;
     if (typeof me.tabSpanOnclickCb === 'function' && me.tabSpanOnclickCb(tab) == false) return;  //the callback is implemented in TabLayout.
-
+    else if (me.container.ontabclick != null) {
+      // Handle the tab panel ontabclick.
+      var returnVal = me.container.ontabclick(tab);
+      if (returnVal == false) return;
+    }
+    
     if (me.container.tabKeys != null) {
       var tabKeysArray = me.container.tabKeys.split(",");
       if (tabKeysArray.length - 1 >= tab) {