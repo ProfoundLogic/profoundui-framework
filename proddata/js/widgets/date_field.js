719c468648b8594e2422ae5bc0f5cb3546e5c5ca
abarge <abarge@profoundlogic.com>
2022-04-12 16:39:51 -0400

    Fixed bug where setting field type removes floating placeholder on all valid input types excluding spinners

--- proddata/js/widgets/date_field.js
+++ proddata/js/widgets/date_field.js
@@ -835,36 +835,17 @@ pui.widgets.add({
         }
       }
     }
+    
   },
 
   globalAfterSetter: function(parms) {
-    // If the date field has a floating placeholder, then properties have been applied to the placeholder div
-    // instead of the actual date field. For certain properties (and this list should be added to), move them to
-    // the inner date field.
+
+    if (parms.propertyName == 'field type' && parms.oldDom && parms.oldDom.floatingPlaceholder != null && parms.dom && parms.dom.floatingPlaceholder == null) {
+      pui.floatPlaceholder(parms.dom);
+    }
+
     if (parms.dom && parms.dom.floatingPlaceholder != null) {
-      var box = parms.dom.floatingPlaceholder.getBox();
-      var propertyName = parms.propertyName;
-      var pnm = getPropertiesNamedModel();
-      var stylename;
-
-      if (pnm[propertyName] && pnm[propertyName].stylename) stylename = pnm[propertyName].stylename;
-      else if (pnm[propertyName] && pnm[propertyName].attribute) stylename = pnm[propertyName].attribute;
-
-      if (stylename) {
-        switch (propertyName) {
-          case "disabled":
-          case "read only":
-            box[stylename] = parms.newValue;
-            parms.dom.removeAttribute(stylename);
-            break;          
-          case "border radius":
-          case "text align":
-          case "height":
-              box.style[stylename] = parms.dom.style[stylename];
-              parms.dom.style[stylename] = "";
-              break;
-        }
-      }
+      pui.movePropertiesFromFloatingPlaceholderDiv(parms);
     }
   }
   
