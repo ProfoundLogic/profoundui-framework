a6918bfa899e34756178e4c1525a08e09efd9ef4
Hugo Cantor <hcantor@profoundlogic.com>
2023-04-06 15:40:13 -0400

    PUI-262. Corrected decimal numbers for DECFMT J

--- proddata/js/runtime/dspf/formatting.js
+++ proddata/js/runtime/dspf/formatting.js
@@ -489,7 +489,13 @@ pui.FieldFormat = {
 
       if (decLength > 0){
         strDec = pui.formatting.rightPad(strDec, decLength, '0');
-        strValue = strInt + (commaDecimal ? ',' : '.') + strDec;
+        if (!commaDecimal) {
+          strValue = strInt + '.' + strDec;
+        }
+        else if (pui.appJob["decimalFormat"] == "J" && strValue.charAt(0) == ",") {
+          strValue = '0' + strValue;
+        }
+        
         if (!obj.noExtraSpaces || obj.noExtraSpaces != "true")
           maxLength++;
         if (commaDecimal) keyFilter += ',';
